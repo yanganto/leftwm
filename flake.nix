881fd12c3c79e97eddba40ae6c0875859852f9b8
Antonio Yang <yanganto@gmail.com>
2023-01-26 16:27:21 +0800

    Add flake for project

--- flake.nix
+++ flake.nix
@@ -1,77 +1,29 @@
 {
+
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
     flake-utils.url = "github:numtide/flake-utils";
-    fenix = {
-      url = "github:nix-community/fenix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    naersk = {
-      url = "github:nix-community/naersk";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
 
-  outputs = { self, fenix, flake-utils, naersk, nixpkgs }:
-    (flake-utils.lib.eachSystem [ "x86_64-linux" "aarch64-linux" ] (system:
+  outputs = { self, rust-overlay, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = nixpkgs.legacyPackages.${system};
-        deps = with pkgs; [
-          xorg.libX11
-          xorg.libXinerama
-        ];
-
-        devToolchain = fenix.packages.${system}.stable;
-
-        leftwm = ((naersk.lib.${system}.override {
-          inherit (fenix.packages.${system}.minimal) cargo rustc;
-        }).buildPackage {
-          name = "leftwm";
-          src = ./.;
-          buildInputs = deps;
-          postFixup = ''
-            for p in $out/bin/left*; do
-              patchelf --set-rpath "${pkgs.lib.makeLibraryPath deps}" $p
-            done
-          '';
-
-          GIT_HASH = self.shortRev or "dirty";
-        });
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+        rust = pkgs.rust-bin.stable.latest.default;
       in
-      rec {
-        # `nix build`
-        packages = {
-          inherit leftwm;
-          default = leftwm;
+      with pkgs;
+      {
+        devShell = mkShell {
+          buildInputs = [
+            rust
+            openssl
+            pkg-config
+          ];
         };
-
-        # `nix run`
-        apps = {
-          leftwm = flake-utils.lib.mkApp {
-            drv = packages.leftwm;
-          };
-          default = apps.leftwm;
-        };
-
-        # `nix develop`
-        devShells.default = pkgs.mkShell
-          {
-            buildInputs = deps ++ [ pkgs.pkg-config pkgs.systemd ];
-            nativeBuildInputs = with pkgs; [
-              gnumake
-              (devToolchain.withComponents [
-                "cargo"
-                "clippy"
-                "rust-src"
-                "rustc"
-                "rustfmt"
-              ])
-              fenix.packages.${system}.rust-analyzer
-            ];
-          };
-      })) // {
-      overlay = final: prev: {
-        leftwm = self.packages.${final.system}.leftwm;
-      };
-    };
+      }
+    );
 }
